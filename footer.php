376b8108deea2a90b6a895e2a464611b7a92a655
mzylka <m.zylka1311@gmail.com>
2020-06-06 12:01:40 +0200

    Add footer menu

--- footer.php
+++ footer.php
@@ -5,7 +5,18 @@
         <div class="footer__links">
             <a href="https://facebook.com"><svg class="footer__fb"><use xlink:href="<?php echo get_template_directory_uri(); ?>/images/sprite.svg#icon-facebook2"></use></svg></a>
         </div>
-        <div class="footer__menus"></div>
+        <div class="footer__menu">
+            <nav class=footer__navigation role="navigation">
+                <?php
+                        wp_nav_menu(
+                            array(
+                                'theme_location' => 'footer',
+                                'menu_class'     => 'footer__navigation-menu',
+                            )
+                        );
+                ?>
+            </nav>
+        </div>
         <div class="footer__copyright">© Copyright strategicznegry.pl. All rights reservred.</div>
     </div>
 </footer>
