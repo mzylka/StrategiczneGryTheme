7a2b2c97dda125e8e01d55a8f45a25ba7b13aaa5
mzylka <m.zylka1311@gmail.com>
2020-06-08 09:32:27 +0200

    Add FB link to footer

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <footer class="footer u-padding-top-big">
     <div class="footer__content container">
         <div class="footer__links">
-            <a href="https://facebook.com"><svg class="footer__fb"><use xlink:href="<?php echo get_template_directory_uri(); ?>/images/sprite.svg#icon-facebook2"></use></svg></a>
+            <a href="https://www.facebook.com/StrategiczneGry/"><svg class="footer__fb"><use xlink:href="<?php echo get_template_directory_uri(); ?>/images/sprite.svg#icon-facebook2"></use></svg></a>
         </div>
         <div class="footer__menu">
             <nav class=footer__navigation role="navigation">
